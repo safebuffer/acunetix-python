bf63cb2fe107ddc09b8c83bd960423e0ad87e2f9
wazehell <wazehell@outlook.com>
2019-08-13 19:11:26 +0000

    i will update more once i have time

---
+++ core.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from requests import get as http_get_request
+from requests import post as http_post_request
+import requests
+requests.packages.urllib3.disable_warnings()
+import json
+
+class ACUX(object):
+    def __init__(self, host=None, api=None, timeout=10):
+        self.apikey = api
+        self.host = str("{}{}".format("https://" if "https://" not in host else "",host))
+        self.timeout = timeout
+        
+    def __headers(self):
+        return {"X-Auth":self.apikey,"content-type": "application/json"} if self.apikey else None
+
+    def __get_request(self,endpoint=None):
+        return http_get_request(str("{}{}".format(self.host, endpoint if endpoint else "/")),headers=self.__headers(),timeout=self.timeout,verify=False)
+    
+    def __post_request(self,endpoint=None,data=None):
+        return http_post_request(str("{}{}".format(self.host, endpoint if endpoint else "/")),data=json.dumps(data),headers=self.__headers(),timeout=self.timeout,verify=False)
+
+    def __json_return(self,data):
+        return json.loads(data)
+
+    def scans(self):
+        req = self.__get_request(endpoint="/api/v1/scans")
+        return self.__json_return(req.text)
+
+    def info(self):
+        req = self.__get_request(endpoint="/api/v1/info")
+        return self.__json_return(req.text)
+
+    def add_target(self,target=None):
+        req = self.__post_request(endpoint="/api/v1/targets",data={"address":target,"description":"xxxx","criticality":"10"})
+        return self.__json_return(req.text)
+
