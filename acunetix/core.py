961f178a03801e1e6ccf82d2897169909d4ebbdd
ahm3delhady <amelhadyy@gmail.com>
2020-06-03 11:21:10 -0700

    commit pre

--- acunetix/core.py
+++ acunetix/core.py
@@ -2,33 +2,44 @@
 from requests import get as http_get_request
 from requests import post as http_post_request
 import requests
+
 requests.packages.urllib3.disable_warnings()
 import json
-
-
-class AXException(Exception):
-    HTTP_ERROR = "HTTP Error "
-    AUTH_ERROR = "Wrong API Key"
-
-
 
 
 class Acunetix(object):
     def __init__(self, host=None, api=None, timeout=10):
         self.apikey = api
-        self.host = str("{}{}".format("https://" if "https://" not in host else "",host))
+        self.host = str(
+            "{}{}".format("https://" if "https://" not in host else "", host)
+        )
         self.timeout = timeout
-        
+
     def __headers(self):
-        return {"X-Auth":self.apikey,"content-type": "application/json"} if self.apikey else None
+        return (
+            {"X-Auth": self.apikey, "content-type": "application/json"}
+            if self.apikey
+            else None
+        )
 
-    def __get_request(self,endpoint=None):
-        return http_get_request(str("{}{}".format(self.host, endpoint if endpoint else "/")),headers=self.__headers(),timeout=self.timeout,verify=False)
-    
-    def __post_request(self,endpoint=None,data=None):
-        return http_post_request(str("{}{}".format(self.host, endpoint if endpoint else "/")),data=json.dumps(data),headers=self.__headers(),timeout=self.timeout,verify=False)
+    def __get_request(self, endpoint=None):
+        return http_get_request(
+            str("{}{}".format(self.host, endpoint if endpoint else "/")),
+            headers=self.__headers(),
+            timeout=self.timeout,
+            verify=False,
+        )
 
-    def __json_return(self,data):
+    def __post_request(self, endpoint=None, data=None):
+        return http_post_request(
+            str("{}{}".format(self.host, endpoint if endpoint else "/")),
+            data=json.dumps(data),
+            headers=self.__headers(),
+            timeout=self.timeout,
+            verify=False,
+        )
+
+    def __json_return(self, data):
         return json.loads(data)
 
     def scans(self):
@@ -39,21 +50,20 @@ class Acunetix(object):
         req = self.__get_request(endpoint="/api/v1/info")
         return self.__json_return(req.text)
 
-    def add_target(self,target=None):
-        req = self.__post_request(endpoint="/api/v1/targets",data={"address":target,"description":"xxxx","criticality":"10"})
+    def add_target(self, target=None):
+        req = self.__post_request(
+            endpoint="/api/v1/targets",
+            data={"address": target, "description": "xxxx", "criticality": "10"},
+        )
         return self.__json_return(req.text)
 
-    def add_and_start(self,target=None):
-        target_id = self.add_target(target=target)['target_id']
+    def add_and_start(self, target=None):
+        target_id = self.add_target(target=target)["target_id"]
         payload = {
-            "target_id":str(target_id),
-            "profile_id":"11111111-1111-1111-1111-111111111111",
-            "schedule":    
-                {"disable":False,
-                "start_date":None,
-                "time_sensitive":False
-                }
-            }
+            "target_id": str(target_id),
+            "profile_id": "11111111-1111-1111-1111-111111111111",
+            "schedule": {"disable": False, "start_date": None, "time_sensitive": False},
+        }
 
-        req = self.__post_request(endpoint="/api/v1/scans",data=payload)
+        req = self.__post_request(endpoint="/api/v1/scans", data=payload)
         return self.__json_return(req.text)
---
+++ exception/axeception.py
@@ -0,0 +1,12 @@
+class AXException(Exception):
+    HTTP_ERROR = "httpError"
+    AUTH_ERROR = "authError"
+    SERVER_RESOURCE = "serverResource"
+
+    def __init__(self, key, message):
+        Exception.__init__(self, message)
+        self.key = key
+
+
+# for raising myErrors (raise AXException), call that when u tryna raise exceptions, bitch!
+# raise AXException()
